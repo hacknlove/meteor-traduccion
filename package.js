40e0359048202ff503b72722218fd8bd56f5f5ac
antonio <antonio@pykiss.com>
2018-09-17 12:43:39 +0200

    preparando para publicar

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 /* global Package */
 
 Package.describe({
-  name: 'ocs:traduccion',
+  name: 'hacknlove:traduccion',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Traducción de textos estáticos y dinámicos',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@hacknlove.github.com:hacknlove/meteor-traduccion.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -15,6 +15,6 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('1.7.0.5')
   api.use('ecmascript')
-  api.use('templating', 'client')
+  api.use('templating@1.0.0', 'client')
   api.mainModule('traduccion.js')
 })
